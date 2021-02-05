f8838d171da1298098a098a085d58b7e39798759
Ashley Ikeh <ashley.ikeh25@gmail.com>
2021-02-05 15:43:14 -0500

    Made sure the props were working correctly by assigning values in the ProjectsPage.

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,16 +1,20 @@
 import React from "react";
 
-const Projects = ({title, description, link, updated, image, alt}) => {
+const Projects = ({title, description, link, updated, image, alt, contribution}) => {
   return (
     <div>
       <div class="card mb-3">
         <img src={image} class="card-img-top" alt={alt} />
         <div class="card-body">
-          <h5 class="card-title">{title}</h5>
+          <h4 class="card-title">{title}</h4>
           <p class="card-text">
           {description} 
           </p>
-          <button>View The Repo on Github<a href={link} target="_blank"></a></button>
+          <h4 class="card-title">Contributions:</h4>
+          <p class="card-text">
+          {contribution} 
+          </p>
+          <button className="btn-lg btn btn-danger">View Github Repo<a href={link} target="_blank"></a></button>
           <p class="card-text">
             <small class="text-muted">{updated}</small>
           </p>
--- src/containers/ProjectsPage/ProjectsPage.jsx
+++ src/containers/ProjectsPage/ProjectsPage.jsx
@@ -11,7 +11,7 @@ const ProjectsPage = () => {
       <div className="container">
         <div className="row">
           <div className="col">
-            <Projects />
+            <Projects title="The Inspirator" description="The Inspirator: an application to help unlock creativity for persons needing a fresh approach. " link="https://github.com/Aikeh2021/The-Inspirator" updated="Last updated 4 months ago" image="" alt="The Inspirator Thumbnail" contribution="I was primarily responsible for implementing the last feature on the website, the film inspiration. I incorporated a button to randomly shuffle an array of movies and pick one to be displayed on the website. With the help of an API from The Movie Database, we were able to attach features like film trailers, descriptions, release dates, and run times for highly rated films from several genres and countries."/>
           </div>
         </div>
       </div>
