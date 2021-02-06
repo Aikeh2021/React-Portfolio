1c4cea3509f6829615edef3f779e279795a3df3f
Ashley Ikeh <ashley.ikeh25@gmail.com>
2021-02-06 10:12:28 -0500

    Filling in content for the project components on the projectsPage. Installed axios. Imported useEffect and useState from react.

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,7 +1,15 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
-const Projects = ({title, description, link, updated, image, alt, contribution}) => {
+const Projects = ({title, description, link, updated, image, alt, contribution,skills}) => {
   return (
+    //   const [thumbNail, setThumbNail] = useState("");
+
+    //   useEffect(() =>{
+    //       axios.get("http://cloudinary.685866416383499:GwJqme4KuTYBMThLfRjZj0lDNFk@elephante-freshamonte").then((response) => {
+
+    //       });
+    //   }, []);
     <div>
       <div class="card mb-3">
         <img src={image} class="card-img-top" alt={alt} />
@@ -10,9 +18,12 @@ const Projects = ({title, description, link, updated, image, alt, contribution})
           <p class="card-text">
           {description} 
           </p>
-          <h4 class="card-title">Contributions:</h4>
+          <h4 class="card-title">Contributions/Skills:</h4>
           <p class="card-text">
           {contribution} 
+          </p>
+          <p class="card-text">
+          {skills} 
           </p>
           <button className="btn-lg btn btn-danger">View Github Repo<a href={link} target="_blank"></a></button>
           <p class="card-text">
