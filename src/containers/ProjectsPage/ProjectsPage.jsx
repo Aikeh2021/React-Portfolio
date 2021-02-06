4ed43fe26c134b08af40ce8cb1c5a9b292985967
Ashley Ikeh <ashley.ikeh25@gmail.com>
2021-02-06 18:28:54 -0500

    Made sure all the links we placed on the proper component and working as expected.

--- src/containers/ProjectsPage/ProjectsPage.jsx
+++ src/containers/ProjectsPage/ProjectsPage.jsx
@@ -30,7 +30,8 @@ const ProjectsPage = () => {
               title="The Inspirator"
               description="The Inspirator: an application to help unlock creativity for persons needing a fresh approach. "
               link="https://github.com/Aikeh2021/The-Inspirator"
-              site=""
+              site="https://mbostwick1.github.io/GT-Project1/"
+              siteName="Visit The Inspirator"
               updated="Last updated October 2020"
               image={Inspirator}
               alt="The Inspirator Thumbnail"
@@ -42,6 +43,7 @@ const ProjectsPage = () => {
               description="The Reserve is a room reservation app that allows college campus library managers to create a log of study rooms. In doing so, it's easier for the manager to keep track of which rooms are in use, empty, or in need of cleaning."
               link="https://github.com/Aikeh2021/conference-room-booker"
               site="https://still-ravine-68015.herokuapp.com/rooms"
+              siteName="Visit The Reserve"
               updated="Last updated December 2020"
               image={Reserve}
               alt="The Reserve Thumbnail"
@@ -52,6 +54,8 @@ const ProjectsPage = () => {
               title="Employee Encrypted Password Generator"
               description="Let us create your password, so you can just worry about getting through the onboarding process!"
               link="https://github.com/Aikeh2021/Employee-Encrypted-Password-Generator"
+              site="https://aikeh2021.github.io/Employee-Encrypted-Password-Generator/"
+              siteName="Visit Employee Encrypted Password Generator"
               updated="Last updated January 2021"
               image={Encrypted}
               alt="The Employee Encrypted Password Generator Thumbnail"
@@ -62,6 +66,8 @@ const ProjectsPage = () => {
               title="The Weather Dashboard"
               description="This site was created for travelers who want the most up-to-date information regarding the weather in any city around the globe."
               link="https://github.com/Aikeh2021/Weather-Dashboard-App"
+              site="https://aikeh2021.github.io/Weather-Dashboard-App/"
+              siteName="Visit The Weather Dashboard"
               updated="Last updated January 2021"
               image={Weather}
               alt="The Weather Dasbhoard Thumbnail"
@@ -72,6 +78,8 @@ const ProjectsPage = () => {
               title="The Workday Scheduler"
               description="With so many of the tools we use migrating to online platforms, I think it's time your planner did the same. The Workday Scheduler is perfect for you to plan the average work shift hour by hour."
               link="https://github.com/Aikeh2021/work-day-scheduler"
+              site="https://aikeh2021.github.io/work-day-scheduler/"
+              siteName="Visit The Workday Scheduler"
               updated="Last updated January 2021"
               image={Scheduler}
               alt="The Worday Scheduler Thumbnail"
@@ -82,6 +90,8 @@ const ProjectsPage = () => {
               title="Budget Tracker"
               description="This service allows users to input a name or short description of a purchase and add or subtract the amount spent. The website provides users with a graph that contains nodes that allow the user to see what was purchased or added to an account over a certain period of time. In addition, this application can be downloaded and used offline!"
               link="https://github.com/Aikeh2021/Budget-Tracker-PWA"
+              site="https://young-peak-40815.herokuapp.com/"
+              siteName="Visit Budget Tracker"
               updated="Last updated January 2021"
               image={Budget}
               alt="Budget Tracker Thumbnail"
@@ -91,7 +101,9 @@ const ProjectsPage = () => {
             <Projects
               title="Employee Directory"
               description="There's nothing more helpful than having a directory of your employees' information complete with a picture of each of their smiling faces! The Employee Directory gives you a way to store information about your staff in a neat and easy to follow website. "
-              link="https://github.com/Aikeh2021/Weather-Dashboard-App"
+              link="https://github.com/Aikeh2021/employee-directory"
+              site="https://aikeh2021.github.io/employee-directory/"
+              siteName="Visit Employee Directory"
               updated="Last updated February 2021"
               image={Directory}
               alt="Employee Directory Thumbnail"
