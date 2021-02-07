972aacbf21b04206e5d3d8c93fdc93e933701fcb
Ashley Ikeh <ashley.ikeh25@gmail.com>
2021-02-07 14:28:40 -0500

    Carousel function is still a WIP. Will continue to work on it at a later date.

--- src/components/Carousel/Carousel.jsx
+++ src/components/Carousel/Carousel.jsx
@@ -1,23 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import Background from "../../Assets/Background.png";
 import Background2 from "../../Assets/Background2.png";
 import Background3 from "../../Assets/Background3.png";
 
 const Carousel = () => {
   let picturesArray = [Background, Background2, Background3];
+  const [index, setIndex] = useState(0);
+
   return (
     <div>
       <div className="Container">
         <div className="row">
           <div className="col-sm-12">
-            <div id="pictureDiv">
+            {/* <div id="pictureDiv">
               {picturesArray.map((item, index) => {
                 return (
                   <div key={index} className="d-block w-100">
-                    <img src={item} alt="Carousel Image"/>
+                    <img src={item} alt="Carousel Image" />
                   </div>
+                  
                 );
               })}
+            </div> */}
+
+            <div class="carousel-item active">
+              <img src={Background} class="d-block w-100" alt="Coding Set Up" />
             </div>
           </div>
         </div>
